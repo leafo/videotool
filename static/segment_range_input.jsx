188efb244f439204a2b76b0c80ebd399e7949fca
leaf corcoran <leafot@gmail.com>
2021-02-23 00:07:22 -0800

    add dragging support for segment input

--- static/segment_range_input.jsx
+++ static/segment_range_input.jsx
@@ -72,12 +72,75 @@ export class SegmentRangeInput extends React.PureComponent {
     }
   }
 
+  onMouseDown(e) {
+    if (e.altKey) {
+      this.startDrag(e)
+      this.inputRef.current.focus()
+      e.preventDefault()
+    }
+  }
+
+  startDrag(e) {
+    let multiplier = 1
+    if (e.shiftKey) {
+      multiplier = 5
+    }
+
+    let getValue = e => {
+      let delta = this.state.dragStartY - e.clientY
+      return this.state.dragStartValue + delta * 1/60 * multiplier
+    }
+
+    let windowUpListener = e => {
+      this.unbindWindowListener()
+      this.setState({
+        editingValue: getValue(e)
+      }, () => this.commitChange())
+    }
+
+    let windowMoveListener = e => {
+      e.preventDefault()
+      let value = getValue(e)
+
+      this.props.setCurrentTime(value)
+      this.setState({
+        editingValue: value
+      })
+    }
+
+    window.addEventListener('mouseup', windowUpListener)
+    window.addEventListener('mousemove', windowMoveListener)
+
+    this.setState({
+      editing: true,
+      dragStartValue: this.props.value,
+      dragStartX: e.clientX,
+      dragStartY: e.clientY,
+      windowMoveListener,
+      windowUpListener,
+    })
+  }
+
+  unbindWindowListener() {
+    if (this.state.windowMoveListener) {
+      window.removeEventListener('mousemove', this.state.windowMoveListener)
+      window.removeEventListener('mouseup', this.state.windowUpListener)
+
+      this.setState({
+        windowMoveListener: null,
+        windowUpListener: null
+      })
+    }
+  }
+
+
   onFocus() {
     this.props.setCurrentTime(this.props.value)
   }
 
   render() {
     return <input
+      ref={this.inputRef ||= React.createRef()}
       class={classNames("segment_range_input", this.props.class, {
         is_invalid: this.props.isInvalid,
         editing: this.state.editing
@@ -88,7 +151,8 @@ export class SegmentRangeInput extends React.PureComponent {
       onKeyDown={this._onKeyDown ||= this.onKeyDown.bind(this)}
       onFocus={this._onFocus ||= this.onFocus.bind(this)}
       onBlur={this._onBlur ||= this.onBlur.bind(this)}
-      />
+      onMouseDown={this._onMouseDown ||= this.onMouseDown.bind(this)}
+    />
   }
 }
 
